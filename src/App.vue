1622020467a3bef4b1650cd45f680fd7e316b638
svyazist2005 <svyazist2005@gmail.com>
2018-11-16 00:03:03 +0200

    Head interface gui  created

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <mainmenu></mainmenu>
+  </div>
+</template>
+
+<script>
+import MainMenu from './components/mainmenu.vue'
+export default{
+  components:{
+    mainmenu:MainMenu
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/mainmenu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="menu">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Stock Trader</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse lefthead" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Stocks</a>
+          </li>
+        </ul>
+      </div>
+      <div class="collapse navbar-collapse righthead" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">End Day <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Save & load
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#"> <b>Founds</b>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.menu{
+  margin:20px;
+}
+li{
+  margin-left:30px;
+  margin-right:30px;
+}
+
+.righthead{
+  margin-left:200px;
+}
+
+</style>
