6a398fb918623bc51c4fa6a0ccc45816aec723eb
svyazist2005 <svyazist2005@gmail.com>
2018-11-20 18:42:05 +0200

    tax mechanism added -30% of funds each day

--- src/components/mainmenu.vue
+++ src/components/mainmenu.vue
@@ -47,7 +47,11 @@ export default{
       this.$store.commit('endDay');
       this.$store.commit('getPortfolioStockValue');
       this.$store.state.gameDay++;
+      this.$store.state.funds=Math.floor(this.$store.state.funds*0.7);
       this.addEventToHistory("Game day "+this.$store.state.gameDay+" end. Balance: "+this.$store.state.funds+"$");
+      this.addEventToHistory("Tax of amount :"+this.$store.state.funds+"$ substracted (30% of fund balance)");
+      this.addEventToHistory("Game day "+this.$store.state.gameDay+" start. Balance: "+this.$store.state.funds+"$");
+
     }
     ,
     saveData(){
