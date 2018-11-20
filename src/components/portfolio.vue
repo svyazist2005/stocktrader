f8acf2ac660f8713d853cd8d752dc50d846923ad
svyazist2005 <svyazist2005@gmail.com>
2018-11-20 16:10:03 +0200

    basic animation fiex <transition-group> used. usaed -leave-active ,-mobe custom classes with transform and transitions

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -4,11 +4,11 @@
       <hr>
 
       <div class="stocks">
-      <transition-group leave-active-class="animated fadeOut" mode="out-in">
-      <div class="stocks" v-for="(stock,index) in $store.state.portfolioStocks" :key='index'>
-          <stock :company='stock.company' :value='stock.value' :amount='stock.amount' :add='false'></stock>
-      </div>
-     </transition-group>
+      <transition-group name='stocksBought' mode="out-in">
+      <li class="stocks" v-for="(stock,index) in $store.state.portfolioStocks" :key='stock' >
+          <stock :company='stock.company' :value='stock.value' :amount='stock.amount' :add='false' ></stock>
+      </li>
+    </transition-group>
       <br>
       </div>
 
@@ -29,5 +29,19 @@ export default{
 }
 </script>
 
-<style>
+<style scoped>
+/*
+.stocksBought-enter{
+transform:rotate(0deg);
+} */
+
+.stocksBought-leave-active{
+transform:scale(0.1);
+position:absolute;
+}
+
+.stocksBought-move{
+transition: all 1s ;
+}
+
 </style>
