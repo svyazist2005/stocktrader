dd21a7cf3d7e09034971987d3bdda496851dfe33
svyazist2005 <svyazist2005@gmail.com>
2018-11-22 11:34:59 +0200

    stock buy check moved to mutations

--- src/components/stock.vue
+++ src/components/stock.vue
@@ -10,7 +10,7 @@
           <span class="input-group-text" id="inputGroup-sizing-sm">Amount</span>
           </div>
           <div class="input">
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model:value='buyAmount' lazy>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model:value='purchase.buyAmount' lazy>
             <button class="btn btn-primary btn-sm" @click="buyStock">Buy</button>
           </div>
         </div>
@@ -40,12 +40,21 @@ export default{
   data:function(){
     return{
     buyAmount:0,
-    sellAmount:0
+    sellAmount:0,
+    purchase:
+    {mode:'buy',
+    company:this.company,
+    sellAmount:this.sellAmount,
+    buyAmount:this.buyAmount,
+    value:this.value}
     }
   },
   methods:{
     buyStock(){
-      if (this.fundsUpdate('buy'))
+      this.purchase.mode='buy';
+      this.updateFunds();
+      if(this.$store.getters.getTransactionState)
+      // if (this.fundsUpdate('buy'))
       {
       this.addEventToHistory(this.buyAmount +" "+this.company+" stocks purchased,"+this.value+ "$ each. Expense: "+Number(this.buyAmount)*Number(this.value)+"$");
 
@@ -60,7 +69,10 @@ export default{
   }
     ,
       sellStock(){
-        if(this.fundsUpdate('sell'))
+        this.purchase.mode='sell';
+        this.updateFunds();
+        if(this.$store.getters.getTransactionState)
+        // if(this.fundsUpdate('sell'))
         {
         this.addEventToHistory(this.amount +" "+this.company+" stocks sold,"+this.value+ "$ each. Income: "+Number(this.amount)*Number(this.value)+"$");
 
@@ -70,25 +82,11 @@ export default{
           if(this.$store.state.portfolioStocks[pack].amount==0)
           this.$store.state.portfolioStocks.splice(pack,1)
         }};
-      }}
-      ,
-
-      fundsUpdate(mode){
-        if (mode=='sell')
-        {for(var pack in this.$store.state.portfolioStocks)
-          {if (this.$store.state.portfolioStocks[pack].company==this.company)
-            {if(this.$store.state.portfolioStocks[pack].amount>=this.sellAmount&&this.sellAmount>0)
-              {this.$store.state.funds=this.$store.state.funds+this.sellAmount*this.value;return true;}
-             else{alert('You are trying to sell more stocks then you have OR minus value');return false;}
-            }
-          }
-        }
-        else
-        if(mode=='buy')
-        {if (this.buyAmount*this.value<=this.$store.state.funds&&this.buyAmount>0)
-        {this.$store.state.funds=this.$store.state.funds-this.buyAmount*this.value;return true;}
-        else{alert('Not enough Funds for purchase OR minus value');return false;}}
+      }},
+      updateFunds(purchase){
+        this.$store.commit('fundsUpdate',purchase=this.purchase);
       }
+
 
 
 
--- src/store.js
+++ src/store.js
@@ -35,7 +35,8 @@ state:{
     funds:10000,
     gameDay:0,
     history:[],
-    amount:0
+    amount:0,
+    transaction:false
 },
 mutations:{
   getPortfolioStockValue:state=>{
@@ -47,9 +48,28 @@ mutations:{
   endDay:state=>{
     for(var i in state.marketStocks)
     state.marketStocks[i].value=Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + Math.ceil(1);;
+  },
+  fundsUpdate:(state,payload)=>{
+    if (payload.mode=='sell')
+    {for(var pack in state.portfolioStocks)
+      {if (state.portfolioStocks[pack].company==payload.company)
+        {if(state.portfolioStocks[pack].amount>=payload.sellAmount&&payload.sellAmount>0)
+          {state.funds=this.$store.state.funds+payload.sellAmount*payload.value;state.transaction=true;}
+         else{alert('You are trying to sell more stocks then you have OR minus value');state.transaction=false;}
+        }
+      }
+    }
+    else
+    if(payload.mode=='buy')
+    {if (payload.buyAmount*payload.value<=state.funds&&payload.buyAmount>0)
+    {state.funds=state.funds-payload.buyAmount*payload.value;state.transaction=true;}
+    else{alert('Not enough Funds for purchase OR minus value');state.transaction=false;}}
   }
 },
 getters:{
+  getTransactionState:state=>{
+    return state.transaction;
+  },
   getFunds:state=>{
     return state.funds;
   },
